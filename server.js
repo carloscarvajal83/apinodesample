6727cedd4f19506e385a064f29d477289d70473f
Carlos Carvajal <carlos.carvajal@aossas.com>
2023-04-21 23:18:24 -0500

    adding gitignore

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 app.use(express.json({limit: '50mb'}));
 app.use(cors());
 
-const port = 3001;
+const port = process.env.PORT | 3001;
 app.listen(port, () => {
     const today = new Date();
     const time = today.getTime().toString();
